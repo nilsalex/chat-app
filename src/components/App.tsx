6feaaa75e709a89168a06706c237928e1787f84d
Nils Alex <nils@famalex.de>
2023-05-19 19:47:01 +0200

    Some styling, introduce types for messages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,23 @@
 import React, { useEffect, useRef, useState } from "react";
 
+interface Message {
+  text: string;
+}
+
+interface MessageSent {
+  messageType: "sent";
+  message: Message;
+}
+
+interface MessageReceived {
+  messageType: "received";
+  message: Message;
+}
+
 export const App = () => {
-  const [messages, setMessages] = useState<Array<string>>([]);
+  const [messages, setMessages] = useState<
+    Array<MessageSent | MessageReceived>
+  >([]);
   const [input, setInput] = useState("");
   const ws = useRef<WebSocket | null>(null);
 
@@ -9,10 +25,22 @@ export const App = () => {
     () => {
       const socket = new WebSocket("ws://localhost:8080/");
 
-      socket.onopen = () => console.log("ws opened!");
-      socket.onclose = () => console.log("ws closed!");
-      socket.onmessage = (event) =>
-        setMessages((currentMessages) => [...currentMessages, event.data]);
+      socket.onopen = () => console.log("ws opened");
+      socket.onclose = () => console.log("ws closed");
+      socket.onmessage = (event) => {
+        console.log("message received");
+        try {
+          const message = JSON.parse(event.data);
+          setMessages((
+            currentMessages,
+          ) => [...currentMessages, {
+            messageType: "received",
+            message,
+          }]);
+        } catch {
+          console.log("could not parse message");
+        }
+      };
 
       ws.current = socket;
 
@@ -22,22 +50,40 @@ export const App = () => {
   );
 
   return (
-    <div>
+    <div style={{ width: "400px", marginLeft: "auto", marginRight: "auto" }}>
       <h1>Let's chat!</h1>
       <div>
-        {messages.map((message) => <div>{message}</div>)}
+        {messages.map((message) => (
+          <div
+            style={{
+              textAlign: message.messageType === "sent" ? "right" : "left",
+            }}
+          >
+            {message.message.text}
+          </div>
+        ))}
       </div>
       <form
         onSubmit={(event) => {
           event.preventDefault();
-          ws.current?.send(input);
+          const message = { text: input };
+          ws.current?.send(JSON.stringify(message));
+          setMessages(
+            (currentMessages) => [...currentMessages, {
+              messageType: "sent",
+              message,
+            }],
+          );
+          console.log("message sent");
           setInput("");
         }}
       >
         <input
           type="text"
           value={input}
-          onChange={(event) => setInput(event.currentTarget.value)}
+          onChange={(event) =>
+            setInput(event.currentTarget.value)}
+          style={{ width: "100%" }}
         />
       </form>
     </div>
