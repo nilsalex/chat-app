1bd536df1b278e55e98b7f026f6e8f806dacd07f
Nils Alex <nils@famalex.de>
2023-05-19 17:49:05 +0200

    Implement basic chat functionality in react

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-export const App = () => (
-  <div>
-    <h1>Hello, world!</h1>
-  </div>
-);
+export const App = () => {
+  const [messages, setMessages] = useState<Array<string>>([]);
+  const [input, setInput] = useState("");
+  const ws = useRef<WebSocket | null>(null);
+
+  useEffect(
+    () => {
+      const socket = new WebSocket("ws://localhost:8080/");
+
+      socket.onopen = () => console.log("ws opened!");
+      socket.onclose = () => console.log("ws closed!");
+      socket.onmessage = (event) =>
+        setMessages((currentMessages) => [...currentMessages, event.data]);
+
+      ws.current = socket;
+
+      return () => socket.close();
+    },
+    [],
+  );
+
+  return (
+    <div>
+      <h1>Let's chat!</h1>
+      <div>
+        {messages.map((message) => <div>{message}</div>)}
+      </div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          ws.current?.send(input);
+          setInput("");
+        }}
+      >
+        <input
+          type="text"
+          value={input}
+          onChange={(event) => setInput(event.currentTarget.value)}
+        />
+      </form>
+    </div>
+  );
+};
